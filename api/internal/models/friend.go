303f96e1bc6ab3e3150dd85d96a6b04ee706a315
tuong.nguyent <tuong.nguyent@PCX-TuongNguyenT.local>
2022-12-26 10:27:01 +0700

    remove db struct tags

--- api/internal/models/friend.go
+++ api/internal/models/friend.go
@@ -3,9 +3,9 @@ package models
 import "time"
 
 type Friend struct {
-	ID        int       `db:"id"`
-	UserID1   int       `db:"user_id_1"`
-	UserID2   int       `db:"user_id_2"`
-	CreatedAt time.Time `db:"created_at"`
-	DeletedAt time.Time `db:"deleted_at"`
+	ID        int
+	UserID1   int
+	UserID2   int
+	CreatedAt time.Time
+	DeletedAt time.Time
 }
