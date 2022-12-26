2f8467433955b37e85eb2596daa0ae1bdb5d0335
tuong.nguyent <tuong.nguyent@PCX-TuongNguyenT.local>
2022-12-26 10:25:50 +0700

    remove db struct tags

--- api/internal/models/user.go
+++ api/internal/models/user.go
@@ -5,9 +5,9 @@ import (
 )
 
 type User struct {
-	ID           int       `db:"id" json:"id"`
-	CreatedAt    time.Time `db:"created_at" json:"created_at"`
-	UpdatedAt    time.Time `db:"updated_at" json:"updated_at"`
-	Email        string    `db:"email" json:"email" validate:"required,email,lte=255"`
-	PasswordHash string    `db:"password_hash" json:"password_hash" validate:"required,lte=255"`
+	ID           int
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+	Email        string
+	PasswordHash string
 }
