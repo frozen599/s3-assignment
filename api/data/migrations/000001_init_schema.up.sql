4242d6aa23ea0d292e6f69b8ba67f9a613b70956
tuong.nguyent <tuong.nguyent@PCX-TuongNguyenT.local>
2022-12-27 10:32:20 +0700

    edit database models

--- api/data/migrations/000001_init_schema.up.sql
+++ api/data/migrations/000001_init_schema.up.sql
@@ -1,29 +1,19 @@
 CREATE TABLE IF NOT EXISTS users (
     id serial PRIMARY KEY,
-    email varchar(256) NOT NULL UNIQUE,
-    password_hash varchar(256) NOT NULL,
+    email text NOT NULL UNIQUE CHECK (email <> ''),
     created_at timestamp with time zone,
     updated_at timestamp with time zone,
     deleted_at timestamp with time zone
 );
 
 
-CREATE TABLE IF NOT EXISTS friends (
+CREATE TABLE IF NOT EXISTS relationships (
     id serial PRIMARY KEY,
     user_id_1 int NOT NULL,
     user_id_2 int NOT NULL,
+    relationship_type TEXT NOT NULL,
     created_at timestamp with time zone,
     updated_at timestamp with time zone,
-    deleted_at timestamp with time zone
-    CONSTRAINT unique_friends UNIQUE(user_id_1, user_id_2),
+    deleted_at timestamp with time zone,
+    CONSTRAINT unique_relationship UNIQUE(user_id_1, user_id_2, relationship_type)
 );
-
-CREATE TABLE IF NOT EXISTS subscribers (
-    id serial PRIMARY KEY,
-    requestor_user_id int,
-    target_user_id int,
-    created_at timestamp with time zone,
-    updated_at timestamp with time zone,
-    deleted_at timestamp with time zone
-);
-
